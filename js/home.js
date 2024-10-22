4cb1d3a078017e7c3590512acad4c6fc68e93037
JulianDelRioS <julian.delrio.s@mail.pucv.cl>
2024-10-22 12:59:36 -0300

    mejoras
    funcion para enviar solicitudes, aceptar solicitudes, admin, etc

--- js/home.js
+++ js/home.js
@@ -1,39 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Obtener el usuario logueado desde localStorage
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    const loggedInUser = users[users.length - 1]; // El último usuario logueado
-
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
     const userBtn = document.querySelector('.user-btn');
     const loginBtn = document.getElementById('loginBtn');
+    const logoutBtn = document.getElementById('logoutBtn');
 
+    // Verificar si hay un usuario logueado
     if (loggedInUser) {
         userBtn.textContent = loggedInUser.username;
         userBtn.style.display = 'inline-block';
         loginBtn.style.display = 'none';
+        logoutBtn.style.display = 'inline-block';
     } else {
         userBtn.style.display = 'none';
         loginBtn.style.display = 'inline-block';
+        logoutBtn.style.display = 'none';
     }
 
-    // Cargar productos desde el localStorage
+    // Lógica para cerrar sesión
+    logoutBtn.addEventListener('click', () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        window.location.href = 'login.html';
+    });
+
+    // Redirigir a login.html cuando se haga clic en el botón de "Iniciar sesión"
+    loginBtn.addEventListener('click', function() {
+        window.location.href = 'login.html'; // Redirigir al usuario a login.html
+    });
+
+    // Cargar productos y solicitudes desde el localStorage
     let products = JSON.parse(localStorage.getItem('products')) || [];
-    console.log('Loaded products:', products); // Verifica los productos cargados
+    let requests = JSON.parse(localStorage.getItem('requests')) || [];
 
     const productsList = document.getElementById('productsList');
     const uploadProductForm = document.getElementById('uploadProductForm');
     const uploadSection = document.getElementById('upload-section');
     const showUploadFormBtn = document.getElementById('showUploadFormBtn');
     const showAllProductsBtn = document.getElementById('showAllProductsBtn');
-    const deleteAllProductsBtn = document.getElementById('deleteAllProductsBtn');
 
-    // Función para renderizar productos en la vista
+    // Función para renderizar productos
     function renderProducts(filteredProducts) {
-        console.log('Rendering products:', filteredProducts); // Verifica los productos a renderizar
         productsList.innerHTML = '';
         filteredProducts.forEach(product => {
             const productDiv = document.createElement('div');
             productDiv.classList.add('product-item');
-            
+
             let imagesHTML = '<div class="carousel-container"><div class="carousel">';
             product.images.forEach(image => {
                 imagesHTML += `<img src="${image}" class="product-image" alt="Imagen del producto">`;
@@ -45,14 +57,28 @@ document.addEventListener('DOMContentLoaded', function() {
                 <h4>${product.name}</h4>
                 <p>Precio: $${product.price}</p>
                 <p>${product.description}</p>
+                <p>Usuario vendedor: ${product.userId}</p> <!-- Mostrar el usuario vendedor -->
             `;
+
+            // Mostrar el botón de eliminar si el usuario es el propietario
+            if (product.userId === loggedInUser.username) {
+                const deleteButton = document.createElement('button');
+                deleteButton.classList.add('delete-btn');
+                deleteButton.setAttribute('data-name', product.name);
+                deleteButton.setAttribute('data-user', product.userId);
+                deleteButton.textContent = 'Eliminar';
+                deleteButton.addEventListener('click', deleteProduct);
+                productDiv.appendChild(deleteButton);
+            }
+
             productsList.appendChild(productDiv);
         });
 
-        // Inicializar los carruseles
+        // Inicializar carruseles
         document.querySelectorAll('.carousel-container').forEach(initCarousel);
     }
 
+    // Función para inicializar los carruseles
     function initCarousel(carouselContainer) {
         const carousel = carouselContainer.querySelector('.carousel');
         const carouselImages = carousel.querySelectorAll('img');
@@ -73,18 +99,21 @@ document.addEventListener('DOMContentLoaded', function() {
             showImage(currentIndex);
         });
 
-        // Inicializar el carrusel mostrando la primera imagen
+        // Mostrar la primera imagen al iniciar
         showImage(currentIndex);
     }
 
+    // Mostrar el formulario de subida de productos
     showUploadFormBtn.addEventListener('click', function() {
         uploadSection.style.display = 'block';
     });
 
+    // Mostrar todos los productos
     showAllProductsBtn.addEventListener('click', function() {
         renderProducts(products);
     });
 
+    // Enviar formulario para solicitar la publicación de un producto
     uploadProductForm.addEventListener('submit', function(event) {
         event.preventDefault();
 
@@ -96,74 +125,73 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let imagesArray = [];
         if (productImagesInput.length > 0) {
-            Array.from(productImagesInput).forEach((file, index) => {
-                if (index < 3) { // Limitar a 3 imágenes
+            const readers = Array.from(productImagesInput).slice(0, 3).map(file => {
+                return new Promise((resolve, reject) => {
                     const reader = new FileReader();
-                    reader.onload = function(e) {
-                        imagesArray.push(e.target.result);
-                        if (imagesArray.length === productImagesInput.length || imagesArray.length === 3) {
-                            saveProduct(productName, productPrice, productCategory, productDescription, imagesArray);
-                        }
-                    };
+                    reader.onload = e => resolve(e.target.result);
+                    reader.onerror = reject;
                     reader.readAsDataURL(file);
-                }
+                });
+            });
+
+            Promise.all(readers).then(images => {
+                imagesArray = images;
+                saveRequest(productName, productPrice, productCategory, productDescription, imagesArray);
             });
         } else {
-            saveProduct(productName, productPrice, productCategory, productDescription, imagesArray);
+            saveRequest(productName, productPrice, productCategory, productDescription, imagesArray);
         }
 
         uploadProductForm.reset();
         uploadSection.style.display = 'none';
     });
 
-    function saveProduct(name, price, category, description, images) {
-        const newProduct = {
+    // Función para guardar la solicitud de publicación de un producto
+    function saveRequest(name, price, category, description, images) {
+        const newRequest = {
             name: name,
             price: price,
             category: category,
             description: description,
-            images: images
+            images: images,
+            userId: loggedInUser.username, // Almacenar el username del usuario que solicita la publicación
+            status: 'pending'
         };
 
-        products.push(newProduct);
-        localStorage.setItem('products', JSON.stringify(products));
-        renderProducts(products);
+        requests.push(newRequest);
+        localStorage.setItem('requests', JSON.stringify(requests));
+        alert('Solicitud de publicación enviada exitosamente.');
     }
 
-    function filterProductsByCategory(category) {
-        if (category) {
-            const filteredProducts = products.filter(product => product.category === category);
-            renderProducts(filteredProducts);
-        } else {
+    // Función para eliminar productos
+    function deleteProduct(event) {
+        const productName = event.target.getAttribute('data-name');
+        const productUserId = event.target.getAttribute('data-user');
+
+        // Permitir eliminar si el usuario es el propietario
+        if (productUserId === loggedInUser.username) {
+            products = products.filter(product => product.name !== productName);
+            localStorage.setItem('products', JSON.stringify(products));
             renderProducts(products);
+            alert('Producto eliminado exitosamente.');
+        } else {
+            alert('No tienes permiso para eliminar este producto.');
         }
     }
 
+    // Filtrar productos por categoría
+    function filterProductsByCategory(category) {
+        const filteredProducts = products.filter(product => product.category === category);
+        renderProducts(filteredProducts);
+    }
+
+    // Manejar la selección de categorías
     document.querySelectorAll('.categories-content a').forEach(categoryLink => {
         categoryLink.addEventListener('click', function(e) {
             e.preventDefault();
             const category = this.getAttribute('data-category');
             filterProductsByCategory(category);
         });
-    });
-
-    // Botón para borrar todos los productos
-    deleteAllProductsBtn.addEventListener('click', function() {
-        // Solicitar clave al usuario
-        const userInput = prompt('Introduce la clave para confirmar la eliminación de todos los productos:');
-
-        if (userInput === '1234') {
-            // Si la clave es correcta, eliminar los productos de localStorage
-            localStorage.removeItem('products');
-            
-            // También limpiar la lista de productos en el DOM
-            products = [];
-            renderProducts(products);
-
-            alert('Todos los productos han sido borrados.');
-        } else {
-            alert('Clave incorrecta. No se han borrado los productos.');
-        }
     });
 
     // Renderizar productos al cargar la página
--- js/register.js
+++ js/register.js
@@ -1,4 +1,4 @@
-// Función de registro de usuario que ya habías agregado
+// Función de registro de usuario
 document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita el envío tradicional del formulario
     
@@ -14,8 +14,18 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         alert('Por favor, ingrese un RUT válido en el formato 12345678-9.');
         return;
     }
+    
+    // Verifica que las contraseñas coincidan
     if (password !== confirmPassword) {
         alert('Las contraseñas no coinciden. Por favor, intenta de nuevo.');
+        return;
+    }
+
+    // Verifica que el nombre de usuario no esté ya en uso
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+    const userExists = users.some(user => user.username === username);
+    if (userExists) {
+        alert('El nombre de usuario ya está en uso. Por favor, elige otro.');
         return;
     }
 
@@ -25,11 +35,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         Rut: Rut,
         email: email,
         password: password
-
     };
-
-    // Obtener usuarios existentes de localStorage o iniciar con un array vacío
-    let users = JSON.parse(localStorage.getItem('users')) || [];
 
     // Agregar el nuevo usuario
     users.push(newUser);
@@ -42,105 +48,3 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     // Redirigir al usuario a la página de inicio de sesión
     window.location.href = 'login.html';
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const daySelect = document.getElementById('day');
-    const monthSelect = document.getElementById('month');
-    const yearSelect = document.getElementById('year');
-    
-    // Rellenar días
-    for (let i = 1; i <= 31; i++) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i;
-        daySelect.appendChild(option);
-    }
-
-    // Rellenar meses
-    const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    months.forEach((month, index) => {
-        const option = document.createElement('option');
-        option.value = index + 1; // Meses de 1 a 12
-        option.textContent = month;
-        monthSelect.appendChild(option);
-    });
-
-    // Rellenar años
-    const currentYear = new Date().getFullYear();
-    for (let i = currentYear; i >= 1900; i--) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = i;
-        yearSelect.appendChild(option);
-    }
-});
-document.addEventListener('DOMContentLoaded', function() {
-    const regionSelect = document.getElementById('region');
-    const comunaSelect = document.getElementById('comuna');
-
-    // Datos de regiones y comunas
-    const regionesComunas = {
-        1: ['Arica', 'Camarones', 'Putre', 'General Lagos'],
-        2: ['Iquique', 'Alto Hospicio', 'Pozo Almonte', 'Pica', 'Huara', 'Camiña', 'Colchane'],
-        3: ['Antofagasta', 'Mejillones', 'Sierra Gorda', 'Taltal', 'Calama', 'Ollagüe', 'San Pedro de Atacama'],
-        4: ['Copiapó', 'Caldera', 'Tierra Amarilla', 'Chañaral', 'Diego de Almagro', 'Vallenar', 'Freirina', 'Huasco'],
-        5: ['La Serena', 'Coquimbo', 'Andacollo', 'La Higuera', 'Paiguano', 'Vicuña', 'Illapel', 'Canela', 'Los Vilos', 'Salamanca', 'Ovalle', 'Combarbalá', 'Monte Patria', 'Punitaqui', 'Río Hurtado'],
-        6: ['Valparaíso', 'Viña del Mar', 'Concón', 'Quintero', 'Puchuncaví', 'Casablanca', 'Juan Fernández', 'Quilpué', 'Villa Alemana', 'Limache', 'Olmué', 'San Antonio', 'Algarrobo', 'Cartagena', 'El Quisco', 'El Tabo', 'Santo Domingo', 'Los Andes', 'Calle Larga', 'Rinconada', 'San Esteban', 'La Ligua', 'Cabildo', 'Papudo', 'Petorca', 'Zapallar', 'Quillota', 'La Calera', 'Hijuelas', 'La Cruz', 'Nogales', 'San Felipe', 'Catemu', 'Llaillay', 'Panquehue', 'Putaendo', 'Santa María'],
-        7: ['Rancagua', 'Codegua', 'Coinco', 'Coltauco', 'Doñihue', 'Graneros', 'Las Cabras', 'Machalí', 'Malloa', 'Mostazal', 'Olivar', 'Peumo', 'Pichidegua', 'Quinta de Tilcoco', 'Rengo', 'Requínoa', 'San Vicente', 'Pichilemu', 'La Estrella', 'Litueche', 'Marchigüe', 'Navidad', 'Paredones', 'San Fernando', 'Chépica', 'Chimbarongo', 'Lolol', 'Nancagua', 'Palmilla', 'Peralillo', 'Placilla', 'Pumanque', 'Santa Cruz'],
-        8: ['Talca', 'Constitución', 'Curepto', 'Empedrado', 'Maule', 'Pelarco', 'Pencahue', 'Río Claro', 'San Clemente', 'San Rafael', 'Cauquenes', 'Chanco', 'Pelluhue', 'Curicó', 'Hualañé', 'Licantén', 'Molina', 'Rauco', 'Romeral', 'Sagrada Familia', 'Teno', 'Vichuquén', 'Linares', 'Colbún', 'Longaví', 'Parral', 'Retiro', 'San Javier', 'Villa Alegre', 'Yerbas Buenas'],
-        9: ['Cobquecura', 'Coelemu', 'Ninhue', 'Portezuelo', 'Quirihue', 'Ránquil', 'Treguaco', 'Bulnes', 'Chillán', 'Chillán Viejo', 'El Carmen', 'Pemuco', 'Quillón', 'San Ignacio', 'Yungay', 'Coihueco', 'Ñiquén', 'San Carlos', 'San Fabián', 'San Nicolás'],
-        10: ['Concepción', 'Coronel', 'Chiguayante', 'Florida', 'Hualqui', 'Lota', 'Penco', 'San Pedro de la Paz', 'Santa Juana', 'Talcahuano', 'Tomé', 'Hualpén', 'Lebu', 'Arauco', 'Cañete', 'Contulmo', 'Curanilahue', 'Los Álamos', 'Tirúa', 'Los Ángeles', 'Antuco', 'Cabrero', 'Laja', 'Mulchén', 'Nacimiento', 'Negrete', 'Quilaco', 'Quilleco', 'San Rosendo', 'Santa Bárbara', 'Tucapel', 'Yumbel', 'Alto Biobío'],
-        11: ['Temuco', 'Carahue', 'Cunco', 'Curarrehue', 'Freire', 'Galvarino', 'Gorbea', 'Lautaro', 'Loncoche', 'Melipeuco', 'Nueva Imperial', 'Padre Las Casas', 'Perquenco', 'Pitrufquén', 'Pucón', 'Saavedra', 'Teodoro Schmidt', 'Toltén', 'Vilcún', 'Villarrica', 'Cholchol', 'Angol', 'Collipulli', 'Curacautín', 'Ercilla', 'Lonquimay', 'Los Sauces', 'Lumaco', 'Purén', 'Renaico', 'Traiguén', 'Victoria'],
-        12: ['Valdivia', 'Corral', 'Lanco', 'Los Lagos', 'Máfil', 'Mariquina', 'Paillaco', 'Panguipulli', 'La Unión', 'Futrono', 'Lago Ranco', 'Río Bueno'],
-        13: ['Puerto Montt', 'Calbuco', 'Cochamó', 'Fresia', 'Frutillar', 'Llanquihue', 'Los Muermos', 'Maullín', 'Puerto Varas', 'Castro', 'Ancud', 'Chonchi', 'Curaco de Vélez', 'Dalcahue', 'Puqueldón', 'Queilén', 'Quellón', 'Quemchi', 'Quinchao', 'Osorno', 'Puerto Octay', 'Purranque', 'Puyehue', 'Río Negro', 'San Juan de la Costa', 'San Pablo', 'Chaitén', 'Futaleufú', 'Hualaihué', 'Palena'],
-        14: ['Coyhaique', 'Lago Verde', 'Aysén', 'Cisnes', 'Guaitecas', 'Cochrane', 'O\'Higgins', 'Tortel', 'Chile Chico', 'Río Ibáñez'],
-        15: ['Punta Arenas', 'Laguna Blanca', 'Río Verde', 'San Gregorio', 'Cabo de Hornos', 'Antártica', 'Porvenir', 'Primavera', 'Timaukel', 'Natales', 'Torres del Paine'],
-        16: ['Santiago', 'Cerrillos', 'Cerro Navia', 'Conchalí', 'El Bosque', 'Estación Central', 'Huechuraba', 'Independencia', 'La Cisterna', 'La Florida', 'La Granja', 'La Pintana', 'La Reina', 'Las Condes', 'Lo Barnechea', 'Lo Espejo', 'Lo Prado', 'Macul', 'Maipú', 'Ñuñoa', 'Pedro Aguirre Cerda', 'Peñalolén', 'Providencia', 'Pudahuel', 'Quilicura', 'Quinta Normal', 'Recoleta', 'Renca', 'San Joaquín', 'San Miguel', 'San Ramón', 'Vitacura', 'Puente Alto', 'Pirque', 'San José de Maipo']
-    };
-    // Rellenar el select de regiones
-    const regiones = [
-        'Región de Arica y Parinacota',
-        'Región de Tarapacá',
-        'Región de Antofagasta',
-        'Región de Atacama',
-        'Región de Coquimbo',
-        'Región de Valparaíso',
-        'Región Metropolitana de Santiago',
-        'Región del Libertador General Bernardo O\'Higgins',
-        'Región del Maule',
-        'Región de Ñuble',
-        'Región del Biobío',
-        'Región de La Araucanía',
-        'Región de Los Ríos',
-        'Región de Los Lagos',
-        'Región de Aysén del General Carlos Ibáñez del Campo',
-        'Región de Magallanes y de la Antártica Chilena'
-    ];
-
-    regiones.forEach((region, index) => {
-        const option = document.createElement('option');
-        option.value = index + 1; // El valor será el número de la región
-        option.textContent = region;
-        regionSelect.appendChild(option);
-    });
-
-    // Actualizar las comunas cuando se seleccione una región
-    regionSelect.addEventListener('change', function() {
-        const regionId = this.value;
-
-        // Limpiar las comunas previas
-        comunaSelect.innerHTML = '<option value="" disabled selected>Comuna</option>';
-
-        // Obtener las comunas de la región seleccionada
-        const comunas = regionesComunas[regionId];
-
-        // Rellenar el select de comunas
-        comunas.forEach(comuna => {
-            const option = document.createElement('option');
-            option.value = comuna;
-            option.textContent = comuna;
-            comunaSelect.appendChild(option);
-        });
-    });
-});
-
