e3470e211084d2f4d1f7668306c3eea5c88035c4
JulianDelRioS <julian.delrio.s@mail.pucv.cl>
2024-11-23 21:02:29 -0300

    captcha

--- myApp/mi-proyecto-backend/server.js
+++ myApp/mi-proyecto-backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./routes/authroutes'); // Importamos las rutas
 const cors = require('cors');
+const axios = require('axios'); // Paquete para hacer solicitudes HTTP
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,39 @@ app.use(cors()); // Habilita CORS para permitir solicitudes desde el frontend
 
 // Middleware para procesar JSON
 app.use(express.json()); // Usa express.json() directamente
+
+// Ruta para verificar el CAPTCHA
+app.post('/api/verify-captcha', async (req, res) => {
+  const { captchaResponse } = req.body;
+
+  if (!captchaResponse) {
+    return res.status(400).json({ error: 'No se proporcionó el CAPTCHA' });
+  }
+
+  try {
+    // Verifica el CAPTCHA con la API de Google
+    const secretKey = '6LfM9IcqAAAAAGhEpudlY1JID4AhnW6UkKApt7rl'; // Reemplaza con tu clave secreta de reCAPTCHA
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify`,
+      null,
+      {
+        params: {
+          secret: secretKey,
+          response: captchaResponse,
+        },
+      }
+    );
+
+    // Si el CAPTCHA es válido
+    if (response.data.success) {
+      return res.json({ message: 'CAPTCHA verificado correctamente' });
+    } else {
+      return res.status(400).json({ error: 'Error de verificación del CAPTCHA' });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Error al verificar el CAPTCHA' });
+  }
+});
 
 // Usar las rutas de autenticación
 app.use('/api/auth', authRoutes); // Prefijo /api/auth
